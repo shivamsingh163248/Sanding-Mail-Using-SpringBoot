4dc47afae725e7b692e27930d6b0fa947c44ff81
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-12-23 19:07:49 +0530

    creating the yml file

--- src/main/java/io/getarray/userservices/resourse/UserResource.java
+++ src/main/java/io/getarray/userservices/resourse/UserResource.java
@@ -1,13 +1,17 @@
 package io.getarray.userservices.resourse;
 
+import io.getarray.userservices.domain.HttpResponse;
 import io.getarray.userservices.domain.user;
 import io.getarray.userservices.service.UserServices;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
@@ -15,10 +19,38 @@ public class UserResource {
  private  final UserServices userServices ;
 
  @PostMapping
-    public ResponseEntity<String> creatUser(@RequestBody user User){
+    public ResponseEntity<HttpResponse> creatUser(@RequestBody user User){
 
+     user newUser = userServices.saveUser(User) ;
+     return ResponseEntity.created(URI.create("")).body(
+             HttpResponse.builder()
+                     .timeStemp(LocalDateTime.now().toString())
+                     .data(Map.of("User", newUser))
+                     .message("User Created")
+                     .status(HttpStatus.CREATED)
+                     .statusCode(HttpStatus.CREATED.value())
+                     .build()
+     );
 
 
  }
+
+ @GetMapping
+ public ResponseEntity<HttpResponse> confirmUserAccount(@RequestParam("token") String token){
+
+  boolean isSuccess =  userServices.varifyToken(token) ;
+  return ResponseEntity.ok().body(
+          HttpResponse.builder()
+                  .timeStemp(LocalDateTime.now().toString())
+                  .data(Map.of("Success", isSuccess))
+                  .message("Account Varyfind")
+                  .status(HttpStatus.OK)
+                  .statusCode(HttpStatus.OK.value())
+                  .build()
+  );
+
+
+ }
+
 
 }
